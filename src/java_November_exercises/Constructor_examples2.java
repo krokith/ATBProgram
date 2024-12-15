a22aa8e2b10c593839f1bc6d265b40b116e2ae77
krokith <krokith20@gmail.com>
2024-12-15 18:28:50 +0530

    temp added

--- src/java_November_exercises/Constructor_examples2.java
+++ src/java_November_exercises/Constructor_examples2.java
@@ -12,14 +12,14 @@ public class Constructor_examples {
 
         Scanner sc = new Scanner(System.in);
 
-
+//temp1
         System.out.println("Enter the car name : ");
         String car_name = sc.nextLine();
         System.out.println("Enter the car model :");
         String car_model = sc.nextLine();
         System.out.println("Enter the car year :");
         int car_year = sc.nextInt();
-
+//temp2
         Constructor_examples t1 = new Constructor_examples(car_name,car_model,car_year);
         System.out.println(t1.car_name);
         System.out.println(t1.car_model);
